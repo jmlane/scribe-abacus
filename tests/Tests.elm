103e82e7546c910c626b027ffe6e406daecafd20
Jonathan M. Lane <jonathan@thinkingbig.net>
2017-02-06 21:40:49 -0400

    Encounter multiplier tests
    All three party size multiplers recommended in the game master guide covered.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -130,4 +130,81 @@ all =
                         )
                 )
             ]
+        , describe "Encounter multiplers"
+            [ describe "Standard size party (3–5)"
+                ([ ( 1, 1 )
+                 , ( 2, 1.5 )
+                 , ( 3, 2 )
+                 , ( 4, 2 )
+                 , ( 5, 2 )
+                 , ( 6, 2 )
+                 , ( 7, 2.5 )
+                 , ( 8, 2.5 )
+                 , ( 9, 2.5 )
+                 , ( 10, 2.5 )
+                 , ( 11, 3 )
+                 , ( 12, 3 )
+                 , ( 13, 3 )
+                 , ( 14, 3 )
+                 , ( 15, 4 )
+                 ]
+                    |> List.map
+                        (\( monsters, multiplier ) ->
+                            test (toString monsters ++ " monsters") <|
+                                \() ->
+                                    Expect.equal multiplier <|
+                                        Encounter.multiplier Standard monsters
+                        )
+                )
+            , describe "Larger than standard (>5)"
+                ([ ( 1, 0.5 )
+                 , ( 2, 1 )
+                 , ( 3, 1.5 )
+                 , ( 4, 1.5 )
+                 , ( 5, 1.5 )
+                 , ( 6, 1.5 )
+                 , ( 7, 2 )
+                 , ( 8, 2 )
+                 , ( 9, 2 )
+                 , ( 10, 2 )
+                 , ( 11, 2.5 )
+                 , ( 12, 2.5 )
+                 , ( 13, 2.5 )
+                 , ( 14, 2.5 )
+                 , ( 15, 3 )
+                 ]
+                    |> List.map
+                        (\( monsters, multiplier ) ->
+                            test (toString monsters ++ " monsters") <|
+                                \() ->
+                                    Expect.equal multiplier <|
+                                        Encounter.multiplier Large monsters
+                        )
+                )
+            , describe "Smaller than standard (<3)"
+                ([ ( 1, 1.5 )
+                 , ( 2, 2 )
+                 , ( 3, 2.5 )
+                 , ( 4, 2.5 )
+                 , ( 5, 2.5 )
+                 , ( 6, 2.5 )
+                 , ( 7, 3 )
+                 , ( 8, 3 )
+                 , ( 9, 3 )
+                 , ( 10, 3 )
+                 , ( 11, 4 )
+                 , ( 12, 4 )
+                 , ( 13, 4 )
+                 , ( 14, 4 )
+                 , ( 15, 5 )
+                 ]
+                    |> List.map
+                        (\( monsters, multiplier ) ->
+                            test (toString monsters ++ " monsters") <|
+                                \() ->
+                                    Expect.equal multiplier <|
+                                        Encounter.multiplier Small monsters
+                        )
+                )
+            ]
         ]
