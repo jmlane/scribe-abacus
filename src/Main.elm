39e66cf80ae8a7c157cb06541f7b5ae86159d46b
Jonathan M. Lane <jonathan@thinkingbig.net>
2017-02-20 06:39:04 -0400

    Setup scaffolding for `beginnerProgram`
    Still just Hello World but using the Elm Architecture.

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,35 @@
+module Main exposing (main)
+
 import Html exposing (..)
 
-main = text "Hello World"
+
+main : Program Never Model Msg
+main =
+    beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    ()
+
+
+model : Model
+model =
+    ()
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    text "Hello World"
