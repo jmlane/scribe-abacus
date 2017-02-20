d003f9f2863483acf30ea6e24cc5006dd9eb72ce
Jonathan M. Lane <jonathan@thinkingbig.net>
2017-02-20 08:50:40 -0400

    Basic Elm Architecture for Party vs Monsters
    Expanding our initial scaffolding with bare minimum user inputs:
    - Single string inputs for party and monsters
    - Custom `onChange` event handler
    - Monster and Party initial view functions

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,87 @@
 module Main exposing (main)
 
 import Html exposing (..)
+import Html.Attributes exposing (type_, value)
+import Html.Events
+import Json.Decode
 
 
 main : Program Never Model Msg
 main =
     beginnerProgram
-        { model = model
+        { model = Model "" ""
         , view = view
         , update = update
         }
 
 
 type alias Model =
-    ()
-
-
-model : Model
-model =
-    ()
+    { party : String
+    , monster : String
+    }
 
 
 type Msg
-    = None
+    = ChangeMonster String
+    | ChangeParty String
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        ChangeMonster value ->
+            { model | monster = value }
+
+        ChangeParty value ->
+            { model | party = value }
 
 
 view : Model -> Html Msg
 view model =
-    text "Hello World"
+    section []
+        [ h1 [] [ text "Encounter Calculator" ]
+        , form []
+            [ viewParty model.party
+            , viewMonsters model.monster
+            , text "Difficulty"
+            ]
+        ]
+
+
+viewParty party =
+    div []
+        [ h3 [] [ text "Party" ]
+        , label []
+            [ input
+                [ --type_ "number"
+                 onChange ChangeParty
+                , value party
+                ]
+                []
+            ]
+        ]
+        -- TODO: dynamic party inputs
+        -- TODO: show XP thresholds
+
+
+viewMonsters monster =
+    div []
+        [ h3 [] [ text "Monsters" ]
+        , label []
+            [ input
+                [ --type_ "number"
+                 onChange ChangeMonster
+                , value monster
+                ]
+                []
+            ]
+        ]
+        -- TODO: dynamic monster inputs
+        -- TODO: show XP values (per monster/per encounter)
+
+
+onChange : (String -> Msg) -> Attribute Msg
+onChange tagger =
+    Html.Events.on "change" <|
+        Json.Decode.map tagger <|
+            Json.Decode.at [ "target" , "value" ] Json.Decode.string
