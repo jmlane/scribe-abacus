a1bc276d1f8087d3c6fa311dd33ef221d9d69e43
Jonathan M. Lane <jonathan@thinkingbig.net>
2017-02-22 10:30:32 -0400

    Updated Model for 1 or more Monsters/Party members
    - `Model.party` and `Model.monsters` are now records with a `first` (required) and `rest` (`Maybe`) property.
    - `Model.monster` and references were renamed to `monsters`
    - `init` now constructs initial `Model` using type alias constructors
    - `viewEncounterMemberLi` function added to create standard encounter member markup
    - `update` altered to compile, *it does not update the model correctly*.

--- src/Main.elm
+++ src/Main.elm
@@ -10,16 +10,28 @@ import Encounter exposing (..)
 main : Program Never Model Msg
 main =
     beginnerProgram
-        { model = Model 0 0 Nothing
+        { model = init
         , view = view
         , update = update
         }
 
 
 type alias Model =
-    { party : Int
-    , monster : Int
+    { party : Party
+    , monsters : Monsters
     , difficulty : Maybe Difficulty
+    }
+
+
+type alias Party =
+    { first : (String, Int)
+    , rest : List (String, Int)
+    }
+
+
+type alias Monsters =
+    { first : (String, Int)
+    , rest : List (String, Int)
     }
 
 
@@ -28,16 +40,28 @@ type Msg
     | ChangeParty String
 
 
+init : Model
+init =
+    Model
+        (Party ("", 0) [])
+        (Monsters ("", 0) [])
+        Nothing
+
+
 update : Msg -> Model -> Model
 update msg model =
+    let
+        monsters = model.monsters
+        party = model.party
+    in
     case msg of
         ChangeMonster value ->
             case String.toInt value of
                 Ok value ->
                     { model
-                    | monster = value
+                    | monsters = { monsters | first = ("", value) } 
                     , difficulty =
-                        getDifficulty [model.party] [value]
+                        getDifficulty [Tuple.second party.first] [value]
                     }
                 Err _ ->
                     model
@@ -46,9 +70,9 @@ update msg model =
             case String.toInt value of
                 Ok value ->
                     { model
-                    | party = value
+                    | party = { party | first = ("", value) }
                     , difficulty =
-                        getDifficulty [value] [model.monster]
+                        getDifficulty [value] [Tuple.second monsters.first]
                     }
                 Err _ ->
                     model
@@ -60,8 +84,8 @@ view model =
         [ h1 [] [ text "Encounter Calculator" ]
         , form []
             [ viewDifficulty model.difficulty
-            , viewParty <| toString model.party
-            , viewMonsters <| toString model.monster
+            , viewParty model.party
+            , viewMonsters model.monsters
             ]
         ]
 
@@ -78,36 +102,42 @@ viewDifficulty difficulty =
             ]
 
 
+viewParty : Party -> Html Msg
 viewParty party =
     div []
         [ h3 [] [ text "Party" ]
-        , label []
-            [ input
-                [ type_ "number"
-                , onChange ChangeParty
-                , value party
-                ]
-                []
+        , ul [] <|
+            viewEncounterMemberLi ChangeParty party.first ::
+            List.map (viewEncounterMemberLi ChangeParty) party.rest
             ]
-        ]
         -- TODO: dynamic party inputs
         -- TODO: show XP thresholds
 
 
-viewMonsters monster =
+viewMonsters : Monsters -> Html Msg
+viewMonsters monsters =
     div []
         [ h3 [] [ text "Monsters" ]
-        , label []
+        , ul [] <|
+            viewEncounterMemberLi ChangeMonster monsters.first ::
+            List.map (viewEncounterMemberLi ChangeMonster) monsters.rest
+        ]
+        -- TODO: dynamic monster inputs
+        -- TODO: show XP values (per monster/per encounter)
+
+
+viewEncounterMemberLi : (String -> Msg) -> (String, Int) -> Html Msg
+viewEncounterMemberLi msg (name, value) =
+    li []
+        [ label []
             [ input
                 [ type_ "number"
-                , onChange ChangeMonster
-                , value monster
+                , onChange msg
+                , Html.Attributes.value <| toString value
                 ]
                 []
             ]
         ]
-        -- TODO: dynamic monster inputs
-        -- TODO: show XP values (per monster/per encounter)
 
 
 onChange : (String -> Msg) -> Attribute Msg
